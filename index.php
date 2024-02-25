4b77b668e5e14c162a01b9578af56aaeb44d505d
yesusgacor <161178339+yesusgacor@users.noreply.github.com>
2024-02-25 08:47:18 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,396 @@
+
+
+
+
+<!DOCTYPE html>
+<html ⚡ lang="id" itemscope="itemscope" itemtype="https://schema.org/WebPage">
+<head>
+    <title>Rekomendasi Situs Slot Paling Gacor Di Tahun 2024!</title>
+    <meta name="title" content="Rekomendasi Situs Slot Paling Gacor Di Tahun 2024!">
+    <meta name="description" content="Disini kami hanya menyediakan situs situs paling gacor di indonesia yang sudah tidak diragukan lagi kegacorannya.Segera Daftar dan raih jackpot maxwin disini!">
+    <meta charset="utf-8">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1">
+    <meta name='Slurp' content='all' />
+    <meta name="author" content="REKOMENDASI SLOT GACOR 2024">
+    <meta name="webcrawlers" content="all" />
+    <meta name="spiders" content="all" />
+    <meta name="allow-search" content="yes" />
+    <meta name="YahooSeeker" content="index,follow" />
+    <meta name="msnbot" content="index,follow" />
+    <meta name="expires" content="never" />
+    <meta name="rating" content="general">
+    <meta name="publisher" content="REKOMENDASI SLOT GACOR 2024">
+    <meta name="googlebot" content="index,follow" />
+    <meta name="robots" content="index,follow" />
+    <meta property="og:url" content="#">
+	<link rel="preload" href="https://ik.imagekit.io/3dgm6pffgn/maxwinnn.jpg?updatedAt=1697134068879" as="image">
+    <link rel="shortcut icon" href="https://i.imgur.com/sM6b3oL.png" type="image/x-icon">
+    <link rel="canonical" href="#" />
+    <link rel="preload" as="script" href="https://cdn.ampproject.org/v0.js">
+    <script async src="https://cdn.ampproject.org/v0.js"></script>
+    <style amp-boilerplate>
+        body {
+            -webkit-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+            -moz-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+            -ms-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+            animation: -amp-start 8s steps(1, end) 0s 1 normal both
+        }
+
+        @-webkit-keyframes -amp-start {
+            from {
+                visibility: hidden
+            }
+
+            to {
+                visibility: visible
+            }
+        }
+
+        @-moz-keyframes -amp-start {
+            from {
+                visibility: hidden
+            }
+
+            to {
+                visibility: visible
+            }
+        }
+
+        @-ms-keyframes -amp-start {
+            from {
+                visibility: hidden
+            }
+
+            to {
+                visibility: visible
+            }
+        }
+
+        @-o-keyframes -amp-start {
+            from {
+                visibility: hidden
+            }
+
+            to {
+                visibility: visible
+            }
+        }
+
+        @keyframes -amp-start {
+            from {
+                visibility: hidden
+            }
+
+            to {
+                visibility: visible
+            }
+        }
+    </style><noscript>
+        <style amp-boilerplate>
+            body {
+                -webkit-animation: none;
+                -moz-animation: none;
+                -ms-animation: none;
+                animation: none
+            }
+        </style>
+    </noscript>
+    <style amp-custom>
+        :root {
+            --white: #eaeaea;
+            --black: #222222;
+            --red: rgb(255, 0, 0);
+            --sky: rgb(39, 223, 255);
+            --blue: rgba(2, 126, 251, 1);
+            --pink: rgb(196, 49, 49);
+            --purple: rgb(189, 6, 6);
+            --gold: rgb(255, 215, 0);
+            --gren-soft: rgb(192, 31, 31);
+            --nulz: #212534;
+            --card-height: 25vh;
+            --card-width: 50vh;
+            --rotate: 132deg;
+        }
+
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        body {
+            background: var(--nulz);
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            align-content: center;
+            min-height: 100vh;
+        }
+
+        .mx-2 {
+            margin: 0 1em 0;
+        }
+
+        .mx-4 {
+            margin: 0 2em 0;
+        }
+
+        .my-2 {
+            margin: 1em 0 1em;
+        }
+
+        .my-4 {
+            margin: 2em 0 2em;
+        }
+
+        .card {
+            background: url('https://i.imgur.com/Vt5QU1T.jpeg') no-repeat right center / cover;
+            width: 200px;
+            height: 200px;
+            padding: 3px;
+            position: relative;
+            border-radius: 10px;
+            justify-content: center;
+            align-items: center;
+            text-align: center;
+            display: flex;
+            font-size: 1.5em;
+            color: rgb(88 199 250 / 0%);
+            cursor: pointer;
+        }
+
+        .card:hover {
+            transition: color 1s;
+        }
+
+        .card:hover:before,
+        .card:hover:after {
+            border-radius: 100px;
+            transform: skewX(2deg) skewY(4deg);
+            background-image: linear-gradient(var(--rotate), #00ccff, #3c67e3 43%, #d400d4);
+            transition: 0.2s all;
+        }
+
+        .card::before {
+            content: "";
+            width: 104%;
+            height: 102%;
+            border-radius: 10px;
+            background-image: linear-gradient(var(--rotate), #00ccff, #3c67e3 43%, #d400d4);
+            position: absolute;
+            z-index: -1;
+            top: -1%;
+            left: -2%;
+            animation: spin 1s linear infinite;
+            transition: 0.2s all;
+        }
+
+        .card::after {
+            position: absolute;
+            content: "";
+            top: calc(var(--card-height) / 6);
+            left: 0;
+            right: 0;
+            z-index: -1;
+            height: 100%;
+            width: 100%;
+            margin: 0 auto;
+            transform: scale(0.8);
+            filter: blur(calc(var(--card-height) / 6));
+            background-image: linear-gradient(var(--rotate), #5ddcff, #3c67e3 43%, #4e00c2);
+            opacity: 1;
+            transition: opacity .5s;
+            animation: spin 2.5s linear infinite;
+        }
+
+        .content {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .btn {
+            width: 100%;
+            padding: 5px 15px;
+            outline: none;
+            border: none;
+        }
+
+        .bg-gradient-purple-pink {
+            background: linear-gradient(123deg, var(--purple) 0%, var(--pink) 100%);
+        }
+
+        .bg-gradient-pink-purple {
+            background: linear-gradient(123deg, var(--pink) 0%, var(--purple) 100%);
+        }
+
+        .bg-gradient-purple-pink:hover {
+            animation: bg1 1s linear infinite;
+            letter-spacing: 5px;
+            transform: scale(1.3);
+            transition: 1s all;
+        }
+        
+        .bg-gradient-pink-purple:hover {
+            animation: bg2 1s linear infinite;
+            letter-spacing: 5px;
+            transform: scale(1.3);
+            transition: 1s all;
+        }
+
+        @keyframes bg1 {
+            0% {
+                background: linear-gradient(321deg, var(--purple) 0%, var(--pink) 100%);
+            }
+
+            100% {
+                background: linear-gradient(123deg, var(--purple) 0%, var(--pink) 100%);
+            }
+        }
+
+        @keyframes bg2 {
+            0% {
+                background: linear-gradient(321deg, var(--pink) 0%, var(--purple) 100%);
+            }
+
+            100% {
+                background: linear-gradient(123deg, var(--pink) 0%, var(--purple) 100%);
+            }
+        }
+
+        .font-white {
+            color: var(--white);
+        }
+
+        .font-red {
+            color: var(--red);
+        }
+
+        .font-sans-serif {
+            font-family: sans-serif;
+        }
+
+        .font-bold {
+            font-weight: 700;
+        }
+
+        .border-white {
+            border: 1px solid var(--white);
+            border-radius: 5px;
+        }
+
+        .fs-2 {
+            font-size: 2vh;
+        }
+
+        .hidden {
+            display: none;
+        }
+
+        .brand {
+            display: flex;
+            flex-direction: column;
+            flex-wrap: nowrap;
+            justify-content: center;
+            align-items: center;
+            text-align: center;
+        }
+
+        .title {
+            transition: 1s;
+            animation: cenat-cenut 1.5s linear infinite;
+        }
+        @keyframes cenat-cenut {
+            0%{
+                transform: scale(1);
+            }
+            50% {
+                transform: scale(1.3);
+            }
+            100% {
+                transform: scale(1);
+            }
+        }
+
+        .sepuh {
+            display: flex;
+            flex-direction: row;
+            flex-wrap: nowrap;
+            white-space: nowrap;
+            justify-content: center;
+            align-items: center;
+            margin-top: 1rem;
+            font-size: 0.75em;
+        }
+
+        .sepuh>span {
+            letter-spacing: 2px;
+            position: relative;
+            display: inline-block;
+            animation: sepuh 0.5s ease-in-out infinite;
+            animation-delay: calc(0.3s * var(--nulz));
+        }
+
+        footer.bawah {
+            position: fixed;
+            bottom: 2%;
+        }
+
+        #pemula {
+            background: transparent;
+        }
+
+        @keyframes sepuh {
+            0% {
+                transform: translateY(0px);
+                color: var(--sky);
+            }
+
+            20% {
+                color: #ff0000;
+                text-shadow: 0 0 20px rgb(255, 0, 128);
+                transform: translateY(-5px);
+            }
+
+            100% {
+                transform: translateY(0px);
+                color: var(--sky);
+            }
+        }
+
+    </style>
+</head>
+
+<body>
+    <div class="content">
+        <div class="card image"></div>
+        <div class="brand font-white my-4">
+            <h1 class="title font-bold font-sans-serif">⚡️ SITUS REKOMENDASI SLOT GACOR 2024  ⚡️</h1><br>
+            <h4 class="pemula font-sans-serif">SLOT GACOR | MIX PARLAY | SPORTBOOK | CASINO</h4>
+            <div class="sepuh font-sans-serif">
+                <span style="--nulz:1;"> Daftar </span>
+				<span style="--nulz:2;"> Depo </span>
+                <span style="--nulz:3;"> JP </span>
+                <span style="--nulz:4;"> Withdraw </span>
+                <span style="--nulz:5;"> Party </span> 
+                <span style="--nulz:6;"> Depo Lagi </span>
+            </div>
+        </div>
+        <div class="btn-grup my-2">
+            <a href="https://gunungsemeru888.site/register" rel="nofollow"><button type="button"
+                    class="btn btn-login bg-gradient-purple-pink font-bold font-white border-white fs-2">🔥 DAFTAR SLOT GACOR 🔥</button></a>
+        </div>
+        <a href="https://gunungsemeru888.site/register" rel="nofollow"><button type="button"
+                class="btn btn-daftar bg-gradient-pink-purple font-bold font-white border-white fs-2">⚡ LOGIN SLOT GACOR ⚡</button></a>
+				
+                <div class="hidden">
+                </div>
+    </div>
+    <footer class="bawah">
+        <span class="font-white font-bold">SOSOK HITAM &copy;2024 - <strong class="font-bold font-red font-sans-serif">JANGAN MARAH YA</strong></span>
+    </footer>
+</body>
+
+</html>
